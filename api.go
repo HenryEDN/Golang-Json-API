8fc8eef9b2a3bf5465808dc65d581c9fae3b9074
Vladislav <vladislav.lion@gmail.com>
2025-03-24 22:51:12 +0100

    Third part

--- api.go
+++ api.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/mux"
 )
@@ -25,8 +26,8 @@ func (s *APIServer) Run(){
 	router := mux.NewRouter()
 
 	router.HandleFunc("/account", makeHTTPHandleFunc(s.handleAccount))
-
 	router.HandleFunc("/account/{id}", makeHTTPHandleFunc(s.handleGetAccountByID))
+	router.HandleFunc("/transfer", makeHTTPHandleFunc(s.handleTransfer))
 
 	log.Println("JSON API server running on port: ", s.listenAddr)
 
@@ -56,12 +57,25 @@ func (s *APIServer) handleAccount(w http.ResponseWriter, r *http.Request) error{
 }
 
 func (s *APIServer) handleGetAccountByID(w http.ResponseWriter, r *http.Request) error{
-	id := mux.Vars(r)["id"]
+	if r.Method == "GET"{
+		id, err := getID(r)
+		if err != nil{
+			return err
+		}
 
-	fmt.Println(id)
+		account, err := s.store.GetAccountById(id)
+		if err != nil{
+			return err
+		}
 
+		return WriteJSON(w, http.StatusOK, account)
+	}
 
-	return WriteJSON(w, http.StatusOK, &Account{})
+	if r.Method == "DELETE"{
+		return s.handleDeleteAccount(w, r)
+	}
+
+	return fmt.Errorf("method not allowed %s", r.Method)
 }
 
 func (s *APIServer) handleCreateAccount(w http.ResponseWriter, r *http.Request) error{
@@ -80,11 +94,25 @@ func (s *APIServer) handleCreateAccount(w http.ResponseWriter, r *http.Request) 
 }
 
 func (s *APIServer) handleDeleteAccount(w http.ResponseWriter, r *http.Request) error{
-	return nil
+	id, err := getID(r)
+	if err != nil{
+		return err
+	}
+
+	if err := s.store.DeleteAccount(id); err != nil{
+		return err
+	}
+	return WriteJSON(w, http.StatusOK, map[string]int{"deleted": id})
 }
 
 func (s *APIServer) handleTransfer(w http.ResponseWriter, r *http.Request) error{
-	return nil
+	transferRequest := new(TransferRequest)
+	if err := json.NewDecoder(r.Body).Decode(transferRequest); err != nil{
+		return err
+	}
+	defer r.Body.Close()
+
+	return WriteJSON(w, http.StatusOK, transferRequest)
 }
 
 func WriteJSON(w http.ResponseWriter, status int, v any) error{
@@ -96,7 +124,7 @@ func WriteJSON(w http.ResponseWriter, status int, v any) error{
 type apiFunc func(http.ResponseWriter, *http.Request) error
 
 type ApiError struct{
-	Error string
+	Error string `json:"error"`
 }
 
 func makeHTTPHandleFunc(f apiFunc) http.HandlerFunc{
@@ -105,4 +133,15 @@ func makeHTTPHandleFunc(f apiFunc) http.HandlerFunc{
 			WriteJSON(w, http.StatusBadRequest, ApiError{Error: err.Error()})
 		}
 	}
+}
+
+func getID(r *http.Request) (int, error){
+	idStr := mux.Vars(r)["id"]
+	id, err := strconv.Atoi(idStr)
+
+	if err != nil{
+		return id, fmt.Errorf("invalid id given %s", idStr)
+	}
+
+	return id, nil
 }
--- storage.go
+++ storage.go
@@ -79,11 +79,19 @@ func (s *PostgresStore) UpdateAccount (*Account) error{
 }
 
 func (s *PostgresStore) DeleteAccount (id int) error{
-	return nil
+	_, err := s.db.Query(`DELETE FROM ACCOUNT WHERE id = $1`, id)
+	return err
 }
 
 func (s *PostgresStore) GetAccountById (id int) (*Account, error){
-	return &Account{},nil
+	rows, err := s.db.Query(`SELECT * FROM ACCOUNT WHERE id = $1`, id)
+	if err != nil{
+		return nil, err
+	}
+	for rows.Next(){
+		return scanIntoAccount(rows)
+	}
+	return nil, fmt.Errorf("account %d not found", id)
 }
 
 func (s *PostgresStore) GetAccounts() ([]*Account, error){
@@ -97,22 +105,28 @@ func (s *PostgresStore) GetAccounts() ([]*Account, error){
 
 	accounts := []*Account{}
 	for rows.Next(){
-		account := new(Account)
-		err := rows.Scan(
-			&account.ID,
-			&account.FirstName,
-			&account.LastName, 
-			&account.Number,
-			&account.Balance,
-			&account.CreatedAt)
+		account, err := scanIntoAccount(rows)
 			
-			if err != nil{
-				return nil, err
-			}
-		
-			accounts = append(accounts, account)
+		if err != nil{
+			return nil, err
+		}
+	
+		accounts = append(accounts, account)
 	}
 
 	return accounts, nil
 
+}
+
+func scanIntoAccount(rows *sql.Rows) (*Account, error){
+	account := new(Account)
+	err := rows.Scan(
+		&account.ID,
+		&account.FirstName,
+		&account.LastName, 
+		&account.Number,
+		&account.Balance,
+		&account.CreatedAt)
+		
+	return account, err
 }
